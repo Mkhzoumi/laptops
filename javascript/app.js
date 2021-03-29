ea9d2940666b390d3c6a2ceb3e44e3dc0b07fb38
Mkhzoumi <zx.hammam@hotmail.com>
2021-03-29 17:18:46 +0300

    css and javascript functions added

--- javascript/app.js
+++ javascript/app.js
@@ -1,35 +1,33 @@
 // first java script test
 
-var usage = prompt('what do you want to use the laptop for?', 'gaming , business , lightweight');
-var alert_message = 'check the end of the page you will find ';
-var images = '';
+function userUsage (){
+    var use = prompt('what do you want to use the laptop for?', 'gaming , business , lightweight');
+    var alert_message = 'check the end of the page you will find ';
 
-while (usage !== 'gaming' && usage !== 'business' && usage !== 'lightweight') {
-    var usage = prompt('please enter one of these three answers?', 'gaming , business , lightweight');
-};
-
-if (usage === 'gaming') {
-    document.write('<h2><a href="https://www.techradar.com/news/mobile-computing/laptops/best-gaming-laptops-top-5-gaming-notebooks-reviewed-1258471">best gaming laptops</a></h1>')
-} else if (usage === 'business') {
-    document.write('<h2><a href="https://www.laptopmag.com/articles/best-business-laptops">best business laptops</a></h1>')
-} else if (usage === 'lightweight') {
-    document.write('<h2><a href="https://www.pcmag.com/picks/the-best-ultraportable-laptops">best lightweight laptops</a></h1>')
-}
-
-var number = prompt('how many ' + usage + ' laptop images you wanna see');
-alert(alert_message + usage + ' laptops')
-
-for (var i = 1; i <= number; i++) {
-    if (usage === 'gaming') {
-        images += '<img src="https://www.lenovo.com/medias/gallery-laptop-4.jpg?context=bWFzdGVyfHJvb3R8NTM5ODF8aW1hZ2UvanBnfGg4MC9oMmUvOTI0NTQyOTIwMjk3NC5qcGd8OTZmOTBkNjQxYTVlODQwNTQwZTRhYzczNWZjZTA3Zjg3NjgxMTA3YzA4OGMzNWQzZDBlYjVhOWY2NmU4ZDM0NQ" width="1440" height="600"/>'
-    } else if (usage === 'business') {
-        images += '<img src="https://wallpaperaccess.com/full/2484044.jpg" width="1440" height="600"/>'
-    } else if (usage === 'lightweight') {
-        images += '<img src="https://cdn.thegentlemansjournal.com/wp-content/uploads/2020/02/lightweight-laptops-hero.jpg" width="1440" height="600"/>'
+    while (use !== 'gaming' && use !== 'business' && use !== 'lightweight') {
+        var use = prompt('please enter one of these three answers?', 'gaming , business , lightweight');
     }
+    alert(alert_message + use + ' laptops');
+
+    return(use);
 }
 
-document.write(images);
+function viewText (){
+    if (usage === 'gaming') {
+        document.write('<h2><a href="https://www.techradar.com/news/mobile-computing/laptops/best-gaming-laptops-top-5-gaming-notebooks-reviewed-1258471">best gaming laptops</a></h1><img src="https://www.lenovo.com/medias/gallery-laptop-4.jpg?context=bWFzdGVyfHJvb3R8NTM5ODF8aW1hZ2UvanBnfGg4MC9oMmUvOTI0NTQyOTIwMjk3NC5qcGd8OTZmOTBkNjQxYTVlODQwNTQwZTRhYzczNWZjZTA3Zjg3NjgxMTA3YzA4OGMzNWQzZDBlYjVhOWY2NmU4ZDM0NQ" width="1440" height="600"/>')
+    } else if (usage === 'business') {
+        document.write('<h2><a href="https://www.laptopmag.com/articles/best-business-laptops">best business laptops</a></h1><img src="https://wallpaperaccess.com/full/2484044.jpg" width="1440" height="600"/>')
+    } else if (usage === 'lightweight') {
+        document.write('<h2><a href="https://www.pcmag.com/picks/the-best-ultraportable-laptops">best lightweight laptops</a></h1><img src="https://cdn.thegentlemansjournal.com/wp-content/uploads/2020/02/lightweight-laptops-hero.jpg" width="1440" height="600"/>')
+    }
+
+}
+
+
+var usage = userUsage();
+
+viewText(usage);
+
+
 
 console.log('he choosed ' + usage + ' laptops');
-console.log('he choosed to see ' + number + ' images');
